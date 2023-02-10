fd14aa478003b56841e987d2e2f592c4c58d17b8
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2023-02-10 16:29:01 +0000

    sets public  attributes of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ This module defines base model needed for the AirBnB console"""
 
+from uuid import uuid4
+from datetime import datetime
+
 class BaseModel:
     """ 
     Class to create ......
@@ -10,4 +13,19 @@ class BaseModel:
         ......
         ......
     """
-    pass
+    def __init__(self) -> None:
+        self.id = str(uuid4())
+        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        pass
+
+    def __str__(self):
+        pass
+
+    def save(self):
+        """ updates the public instance attribute updated_at with the current datetime"""
+        pass
+    
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        pass
