14c0b8e079055a594a84378a964d2afae1debe1d
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2023-02-10 17:02:39 +0000

    Creates functional BaseModel per prelimenary functions

--- models/base_model.py
+++ models/base_model.py
@@ -19,13 +19,16 @@ class BaseModel:
         self.updated_at = datetime.now() #.strftime("%Y-%m-%dT%H:%M:%S.%f")
         pass
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now() #.strftime("%Y-%m-%dT%H:%M:%S.%f")
     
-    def to_dict(self):
+    def to_dict(self) -> dict:
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
-        pass
+        hld_dict = dict({"__class__":self.__class__.__name__}, **self.__dict__)
+        hld_dict["updated_at"] = hld_dict["updated_at"].isoformat()
+        hld_dict["created_at"] = hld_dict["created_at"].isoformat()
+        return hld_dict 
