9d43a02df066acbe5eaf1a6b7a7faacaa268901b
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2023-02-10 11:18:12 +0000

    Starts the structure for the base model

---
+++ models/base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module defines base model needed for the AirBnB console"""
+
+class BaseModel:
+    """ 
+    Class to create ......
+    
+    Methods:
+        ......
+        ......
+        ......
+    """
+    pass
